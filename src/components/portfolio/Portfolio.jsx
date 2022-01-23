0b934f372a81fd57951838f24eebbf6da7616d9e
jerrytc7 <jerrykondner@gmail.com>
2022-01-23 15:39:46 -0500

    adding data.js as well as completing portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,96 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
+import {
+  featuredPortfolio,
+  vanillajsPortfolio,
+  reactPortfolio,
+  rubyPortfolio,
+  rubyonrailsPortfolio,
+  reactandrailsPortfolio,
+} from "../../data";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const [data, setData] = useState([]);
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "vanillajs",
+      title: "Vanilla JS Apps",
+    },
+    {
+      id: "react",
+      title: "React Apps",
+    },
+    {
+      id: "ruby",
+      title: "Ruby Apps",
+    },
+    {
+      id: "rubyonrails",
+      title: "Ruby on Rails Apps",
+    },
+    {
+      id: "reactandrails",
+      title: "React and Rails Apps",
+    },
+  ];
+
+  useEffect(()=>{
+
+    switch(selected){
+      case "featured":
+        setData(featuredPortfolio)
+        break;
+      case "vanillajs":
+        setData(vanillajsPortfolio)
+        break;
+      case "react":
+        setData(reactPortfolio)
+        break;
+      case "ruby":
+        setData(rubyPortfolio)
+        break;
+      case "rubyonrails":
+        setData(rubyonrailsPortfolio)
+        break;
+      case "reactandrails":
+        setData(reactandrailsPortfolio)
+        break;
+        default:
+          setData(featuredPortfolio)
+    }
+
+  },[selected])
+
   return (
-  <div className="portfolio" id="portfolio">
-  </div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        {data.map(d=>(
+        <div className="item">
+          <img
+            src={d.img}
+            alt=""
+          />
+          <h3>{d.title}</h3>
+        </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const featuredPortfolio = [
+    {
+        id: 1,
+        title: "FB Demo App",
+        img: "https://www.citypng.com/public/uploads/preview/-11595350631lczyog2lld.png",
+    },
+    {
+        id: 2,
+        title: "Instagram Demo App",
+        img: "https://igram.io/static/img/instagram-image.jpg",
+    },
+    {
+        id: 3,
+        title: "MLB Roster & HR Search",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png",
+    },
+];
+
+export const vanillajsPortfolio = [
+    {
+        id: 1,
+        title: "MLB Roster & HR Search",
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png",
+    },
+];
+
+export const reactPortfolio = [
+    {
+        id: 1,
+        title: "Instagram Demo App",
+        img: "https://igram.io/static/img/instagram-image.jpg",
+    },
+];
+
+export const rubyPortfolio = [
+    {
+        id: 1,
+        title: "Video Game Organizer",
+        img: "https://assets.unenvironment.org/s3fs-public/inline-images/Play_1_Photo_by_Pixahive.jpg",
+    },
+];
+
+export const rubyonrailsPortfolio = [
+    {
+        id: 1,
+        title: "Student & Teacher Roster Organizer",
+        img: "https://www.graduateprogram.org/wp-content/uploads/2019/09/May-3-The-Importance-of-Building-the-Teacher-Student-Relationship_web.jpg",
+    },
+];
+
+export const reactandrailsPortfolio = [
+    {
+        id: 1,
+        title: "FB Demo App",
+        img: "https://www.citypng.com/public/uploads/preview/-11595350631lczyog2lld.png",
+    },
+];
