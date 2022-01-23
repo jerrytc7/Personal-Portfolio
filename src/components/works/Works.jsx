86c680c0775d26c5305aa8bab281c2be9c8aa0c4
jerrytc7 <jerrykondner@gmail.com>
2022-01-23 16:36:56 -0500

    adding to works component and adding styling

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,32 @@
-import "./works.scss"
+import "./works.scss";
 
 export default function Works() {
   return (
-  <div className="works" id="works"> 
-
-  </div>
-  )
+    <div className="works" id="works">
+      <div className="slider">
+        <div className="container">
+          <div className="item">
+            <div className="left">
+              <div className="leftContainer">
+                <div className="imgContainer">
+                  <img src="assets/mobile.png" alt="" />
+                </div>
+                <h2>Title</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Consequatur veritatis aliquam sed vitae, distinctio dicta.
+                </p>
+                <span>Projects</span>
+              </div>
+            </div>
+            <div className="right">
+              <img src="https://support.brightervision.com/wp-content/uploads/2019/05/background-color.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" />
+      <img src="assets/arrow.png" className="arrow right" alt="" />
+    </div>
+  );
 }
