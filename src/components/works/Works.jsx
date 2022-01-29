7e661c4360eadee7551fa7263b09c71f2ed17869
jerrytc7 <jerrykondner@gmail.com>
2022-01-29 00:18:34 -0500

    works slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,32 +1,62 @@
+import { useState } from "react";
 import "./works.scss";
 
 export default function Works() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: 1,
+      title: "FB Demo App",
+      img: "https://www.citypng.com/public/uploads/preview/-11595350631lczyog2lld.png",
+    },
+    {
+      id: 2,
+      title: "Instagram Demo App",
+      img: "https://igram.io/static/img/instagram-image.jpg",
+    },
+    {
+      id: 3,
+      title: "MLB Roster & HR Search",
+      img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Major_League_Baseball_logo.svg/1200px-Major_League_Baseball_logo.svg.png",
+    },
+  ];
+
+  const handleClick = (way)=>{
+    way === "left" 
+    ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2)
+    : setCurrentSlide(currentSlide<data.length -1 ?currentSlide + 1 : 0);
+  };
   return (
     <div className="works" id="works">
-      <div className="slider">
-        <div className="container">
-          <div className="item">
-            <div className="left">
-              <div className="leftContainer">
-                <div className="imgContainer">
-                  <img src="assets/mobile.png" alt="" />
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.img} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
                 </div>
-                <h2>Title</h2>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Consequatur veritatis aliquam sed vitae, distinctio dicta.
-                </p>
-                <span>Projects</span>
+              </div>
+              <div className="right">
+                <img
+                  src="https://support.brightervision.com/wp-content/uploads/2019/05/background-color.png"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="right">
-              <img src="https://support.brightervision.com/wp-content/uploads/2019/05/background-color.png" alt="" />
-            </div>
           </div>
-        </div>
+        ))}
       </div>
-      <img src="assets/arrow.png" className="arrow left" alt="" />
-      <img src="assets/arrow.png" className="arrow right" alt="" />
+      <img src="assets/arrow.png" className="arrow left" alt="" onClick={() => handleClick("left")}/>
+      <img src="assets/arrow.png" className="arrow right" alt="" onClick={() => handleClick("right")} />
     </div>
   );
 }
